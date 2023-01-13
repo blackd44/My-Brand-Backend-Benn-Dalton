0f8c5d09e959e69e6f49a7e99f742d71cfaa2c6c
blackd44 <irabd44@gmail.com>
2023-01-13 17:41:27 +0200

    removing babelrc and moch test

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { } from 'dotenv/config'
-import testMessages from "./api/messages.spec.js";
-
-const PORT = process.env.SERVER_PORT
-
-const server = 'http://localhost:' + PORT
-
-testMessages(server)
