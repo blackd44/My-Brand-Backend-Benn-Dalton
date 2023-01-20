b856ab13ae494abb9cb0f2cbf171358b82e17b32
blackd44 <irabd44@gmail.com>
2023-01-20 17:14:16 +0200

    changing bd connection way

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ import app from "./app.js"
 // starting the server
 const PORT = process.env.SERVER_PORT || 4000
 
-// while()
-let first = true
-while (first) {
-    if (app !== undefined) {
-        first = false
-        app.listen(PORT, () => {
-            console.log(`listening to port`, PORT)
-        })
-    }
-}
+await app.listen(PORT, () => {
+    console.log(`listening to port`, PORT)
+})
+
+export default app
