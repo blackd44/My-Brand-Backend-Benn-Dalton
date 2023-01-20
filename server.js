3323bb34ed7da8cc00bc9845082b6afbecdd5d89
blackd44 <irabd44@gmail.com>
2023-01-20 16:08:53 +0200

    changing errors from the server

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ import app from "./app.js"
 // starting the server
 const PORT = process.env.SERVER_PORT || 4000
 
-app.listen(PORT, () => {
-    console.log(`listening to port`, PORT)
-})
+// while()
+let first = true
+while (app == undefined || first) {
+    if (app !== undefined) {
+        app.listen(PORT, () => {
+            console.log(`listening to port`, PORT)
+        })
+    }
+    first = false
+}
