cfa36cd55383b14f0463f0601d45909ec9b61b96
blackd44 <irabd44@gmail.com>
2023-01-22 21:56:32 +0200

    changing listening way

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ import app from "./app.js"
 // starting the server
 const PORT = process.env.SERVER_PORT || 4000
 
-// while()
-let first = true
-while (first) {
-    if (app !== undefined) {
-        app.listen(PORT, () => {
-            console.log(`listening to port`, PORT)
-            first = false
-        })
-    }
-}
+await app.listen(PORT, () => {
+    console.log(`listening to port`, PORT)
+})
+
+export default app
